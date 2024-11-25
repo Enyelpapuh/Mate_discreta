39110859c7ec4caf0da9485e8963820df129544f
Enyelp <vegebalto@gmail.com>
2024-11-25 12:30:54 -0600

    title update

--- LAB_FINAL_DISCRETAS/src/App.jsx
+++ LAB_FINAL_DISCRETAS/src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 import PART1 from './pages/PART1'
 import PART2 from './pages/PART2'
 import Main from './pages/Main'
+import { useEffect } from 'react'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
+
+  useEffect(() => {
+    document.title = "LAB Final Discretas"
+  });
 
   return (
     <>
